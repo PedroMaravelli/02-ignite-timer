8d1f733fae9bd5e0b6eada59112b6ea0982496b8
PedroMaravelli <phmaravelli123@gmail.com>
2023-03-06 17:43:48 -0300

    add new page home

--- src/layout/DefaultLayout/styles.ts
+++ src/layout/DefaultLayout/styles.ts
@@ -2,9 +2,10 @@ import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
   max-width: 74rem;
-  height: calc(100vh - 10rem);
+  height: calc(100vh);
   padding: 2.5rem;
   margin: 5rem auto;
+  border-radius: 8px;
 
   display: flex;
   flex-direction: column;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,59 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinuteAmountInput,
+  Separator,
+  StartCountdowmButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="taskName">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            placeholder="Dê um nome para o seu projeto"
+            id="taskName"
+            list="task-suggestions"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto Ignite"></option>
+            <option value="Projeto DH"></option>
+            <option value="Projeto Inglês"></option>
+            <option value="Projeto Matemática"></option>
+
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinuteAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdowmButton disabled={false} type="submit">
+          <Play size={24}></Play>
+          Começar
+        </StartCountdowmButton>
+      </form>
+    </HomeContainer>
+  )
+}
